7d9d3cc1f5615ab71956ecb258605c36745d2c39
unknown <mihinduwijesena@gmail.com --global user.name mihiHarshana>
2014-11-11 07:46:23 +0100

    Added all new files from other computer

--- src/com/example/informationsystem/MainActivity.java
+++ src/com/example/informationsystem/MainActivity.java
@@ -7,84 +7,73 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
-
-
+ 
 public class MainActivity extends ActionBarActivity {
+	 public final static String LOG_IN_USER = "com.example.informationsystem.LOGINUSER";
 public DatabaseHandler db = new DatabaseHandler(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-//        Button btnLogin = (Button)  findViewById(R.id.btnLogin);
-//        
-//        btnLogin.setOnClickListener( new View.OnClickListener() {
-//			
-//			@Override
-//			public void onClick(View v) {
-//				// TODO Auto-generated method stub
-//				Intent intent = new Intent(MainActivity.this, AddPatientData.class);
-//				
-//				
-//				
-//				
-//				Log.d("Strating second one", "What will happen'");
-//				MainActivity.this.startActivity(intent);
-//				
-//				
-//				
-//			}
-//		});
-    }
-
-
+		if (db.getUserCount() ==0  ) {
+			db.addDefaultUser("admin", "admin", "admin");
+			Log.d("User Admin aded", "Added user admin");
+		}
+    }      
+     
+   
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
         return true;
-    }
-
+    }  
+  
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.action_settings) {  
+        	
+        	
+        	
+        	
+            return true; 
         }
         return super.onOptionsItemSelected(item);
     }
-         
+                   
     public void Login(View view) {
-    	  
     	   
+            
      	EditText txtUName = (EditText) findViewById(R.id.txtUserName);
      	EditText txtPword =(EditText) findViewById(R.id.txtPasswprd);
 //     	txtUName.setText("admin");
 //     	txtPword.setText("admin");
     	
-    	if (txtUName.getText().toString().matches("")) {
+    	if (txtUName.getText().toString().matches("")) { 
     		//lblUName.setText("Invalid User Name");
     		Toast.makeText(this, "User Name cannot be blank" , Toast.LENGTH_SHORT).show();
     		
     	} else if (txtPword.getText().toString().matches("")) {
     		Toast.makeText(this, "Password cannot be blank" , Toast.LENGTH_SHORT).show();
     	}
-    	else { 
-    		
+    	else {     
+    
+    
     		Users users = db.getUser(txtUName.getText().toString());
-    		
     		if (users != null) {
     			
     			if (txtUName.getText().toString().matches(users.getUname())) {
+    				Log.d("Password is ", users.getUpword());
     				if (txtPword.getText().toString().matches(users.getUpword())) {
-    					Intent intent  = new Intent (this, AddPatientData.class);
+    					Intent intent  = new Intent (this, PatientView.class);
+    				//	intent.putExtra("LogedinUser", txtUName.getText().toString());
     					startActivity(intent);
     				} else {
     					Toast.makeText(this, "Incorrect password" , Toast.LENGTH_SHORT).show();
@@ -92,7 +81,7 @@ public DatabaseHandler db = new DatabaseHandler(this);
     			} else {
     				Toast.makeText(this, "Incorrect User name " , Toast.LENGTH_SHORT).show();
     			}
-    			
+    			  
     		}  else {
     			Toast.makeText(this, "User Not found" , Toast.LENGTH_SHORT).show();
     			txtPword.setText("");
